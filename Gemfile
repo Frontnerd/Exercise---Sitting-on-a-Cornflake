ba83ab44c8a6aae93e87f333f06593b8f99ffb3b
Tommaso Romito <tommaso@devolute.org>
2014-07-25 10:17:55 +0200

    validate Idea file upload MIME type

--- Gemfile
+++ Gemfile
@@ -4,7 +4,7 @@ source 'https://rubygems.org'
 # ruby '2.1.2'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
-gem 'rails', '4.0.2'
+gem 'rails', github: 'rails/rails', branch: '4-0-stable'
 
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3'
--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,6 +1,9 @@
 class Idea < ActiveRecord::Base
+  # paperclip
   has_attached_file :attachment, :default_url => "/images/:style/missing.png"
-  do_not_validate_attachment_file_type :attachment
+  validates_attachment_content_type :attachment, :content_type => [ 'application/mp3', 'application/x-mp3', 'audio/mpeg', 'audio/mp3' ],
+                                    :message => 'file must be of filetype .mp3'
+  validates_attachment_size :attachment, :less_than => 10.megabytes
   
   # user association
   belongs_to :user
